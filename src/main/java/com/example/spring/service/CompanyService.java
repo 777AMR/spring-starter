39b4ee2b1a5d979632a4464f5732d1f9c0b61ea5
777AMR <meyram.ramazanov@gmail.com>
2022-07-10 11:37:22 +0600

    Properties Injection

---
+++ src/main/java/com/example/spring/service/CompanyService.java
@@ -0,0 +1,10 @@
+package com.example.spring.service;
+
+public class CompanyService {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
