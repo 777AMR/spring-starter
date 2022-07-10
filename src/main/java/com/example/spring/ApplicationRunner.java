1b9c3b8f83f33b5d6e86263249bd0442d947ac7a
777AMR <meyram.ramazanov@gmail.com>
2022-07-10 18:04:34 +0600

    Lifecycle Callbacks

--- src/main/java/com/example/spring/ApplicationRunner.java
+++ src/main/java/com/example/spring/ApplicationRunner.java
@@ -7,12 +7,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 //        clazz -> String -> Map<String, Object>
-        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
-        System.out.println(connectionPool);
+            ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+            System.out.println(connectionPool);
 
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
