bf55c548180b3fe16ddcb3a4e1285c311283760c
777AMR <meyram.ramazanov@gmail.com>
2022-07-10 10:44:29 +0600

    Constructor Injection

--- src/main/java/com/example/spring/ApplicationRunner.java
+++ src/main/java/com/example/spring/ApplicationRunner.java
@@ -1,26 +1,15 @@
 package com.example.spring;
 
 import com.example.spring.database.pool.ConnectionPool;
-import com.example.spring.database.repository.CompanyRepository;
-import com.example.spring.database.repository.UserRepository;
-import com.example.spring.ioc.Container;
-import com.example.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
+//        clazz -> String -> Map<String, Object>
+        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+        System.out.println(connectionPool);
 
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
-        //TODO 10.07.2022 userService
     }
 }
