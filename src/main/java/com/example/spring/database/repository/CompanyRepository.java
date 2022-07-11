b1b27a73408192257647366c32ec8c0e9e8bc4d7
777AMR <meyram.ramazanov@gmail.com>
2022-07-11 18:00:04 +0600

    Custom BeanPostProcessor 1.

--- src/main/java/com/example/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/example/spring/database/repository/CompanyRepository.java
@@ -1,15 +1,10 @@
 package com.example.spring.database.repository;
 
+import com.example.spring.bpp.InjectBean;
 import com.example.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
+    @InjectBean
+    private ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
 }
