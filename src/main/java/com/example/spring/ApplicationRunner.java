c2516b0b479012a9c8309c39d991855647939c50
777AMR <meyram.ramazanov@gmail.com>
2022-07-11 22:06:51 +0600

    Custom BeanPostProcessor 2.

--- src/main/java/com/example/spring/ApplicationRunner.java
+++ src/main/java/com/example/spring/ApplicationRunner.java
@@ -2,6 +2,7 @@ package com.example.spring;
 
 import com.example.spring.database.pool.ConnectionPool;
 import com.example.spring.database.repository.CompanyRepository;
+import com.example.spring.database.repository.CrudRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -9,11 +10,11 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 //        clazz -> String -> Map<String, Object>
-            ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+            var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
 
-            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-            System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CrudRepository.class);
+            System.out.println(companyRepository.findById(1));
         }
     }
 }
--- src/main/java/com/example/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/example/spring/database/repository/CompanyRepository.java
@@ -1,10 +1,34 @@
 package com.example.spring.database.repository;
 
+import com.example.spring.bpp.Auditing;
 import com.example.spring.bpp.InjectBean;
+import com.example.spring.bpp.Transaction;
+import com.example.spring.database.entity.Company;
 import com.example.spring.database.pool.ConnectionPool;
 
-public class CompanyRepository {
-    @InjectBean
-    private ConnectionPool connectionPool;
+import javax.annotation.PostConstruct;
+import java.util.Optional;
 
+@Transaction
+@Auditing
+public class CompanyRepository implements CrudRepository<Integer, Company> {
+
+    @InjectBean
+    public ConnectionPool connectionPool;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("init company repository");
+    }
+
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("findById method...");
+        return Optional.of(new Company(id));
+    }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("delete method...");
+    }
 }
