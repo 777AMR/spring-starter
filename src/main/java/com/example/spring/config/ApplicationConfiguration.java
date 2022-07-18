f3307214a4a3c7c756506a27617c60111bf3ba23
777AMR <meyram.ramazanov@gmail.com>
2022-07-18 21:43:09 +0600

    @Bean Part 1.

--- src/main/java/com/example/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/example/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.example.spring.config;
 
+import com.example.spring.database.pool.ConnectionPool;
 import com.example.spring.database.repository.CrudRepository;
+import com.example.spring.database.repository.UserRepository;
 import com.example.web.config.web.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -18,5 +22,14 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
         })
 public class ApplicationConfiguration {
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
 
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
 }
